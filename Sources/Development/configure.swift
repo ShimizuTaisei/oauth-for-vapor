0c5f37e89d6753e782467fb83e77113c62965840
ShimizuTaisei <74182566+ShimizuTaisei@users.noreply.github.com>
2024-01-21 17:44:07 +0900

    Add migrations to Development target.

--- Sources/Development/configure.swift
+++ Sources/Development/configure.swift
@@ -1,4 +1,5 @@
 import NIOSSL
+import VaporOAuth
 import Fluent
 import FluentMySQLDriver
 import Leaf
@@ -9,6 +10,18 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    #if DEBUG
+    var tlsConfiguration = TLSConfiguration.makeClientConfiguration()
+    tlsConfiguration.certificateVerification = .none
+    app.databases.use(DatabaseConfigurationFactory.mysql(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        tlsConfiguration: tlsConfiguration
+    ), as: .mysql)
+    #else
     app.databases.use(DatabaseConfigurationFactory.mysql(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
@@ -16,11 +29,20 @@ public func configure(_ app: Application) async throws {
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
     ), as: .mysql)
+    #endif
 
 
     app.views.use(.leaf)
-
     
+    app.migrations.add(CreateUsers())
+    app.migrations.add(CreateOAuthScopes())
+    app.migrations.add(CreateOAuthClients())
+    app.migrations.add(CreateOAuthAccessTokens(userTableName: "users", userTableIdFiled: "id"))
+    app.migrations.add(CreateOAuthRefreshTokens(userTableName: "users", userTableIdField: "id"))
+    app.migrations.add(CreateOAuthAuthorizationCodes(userTableName: "users", userTableIdFiled: "id"))
+    app.migrations.add(CreateOAuthAccessTokenScopes())
+    app.migrations.add(CreateOAuthRefreshTokenScopes())
+    app.migrations.add(CreateOAuthAuthorizationCodeScopes())
 
     // register routes
     try routes(app)

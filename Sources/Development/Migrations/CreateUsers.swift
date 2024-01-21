eb8d589493fedae4bee53d01fe2fdd56e86617b4
ShimizuTaisei <shimizu-taisei@shimizu-taisei.com>
2024-01-21 17:44:07 +0900

    Add migrations to Development target.

--- Sources/Development/Migrations/CreateUsers.swift
+++ Sources/Development/Migrations/CreateUsers.swift
@@ -1,9 +1,29 @@
 //
-//  File.swift
-//  
+//  CreateUsers.swift
+//
 //  
 //  Created by Shimizu Taisei on 2024/01/21.
 //  
 
 
 import Foundation
+import Fluent
+
+struct CreateUsers: AsyncMigration {
+    var name: String = "users"
+    
+    func prepare(on database: Database) async throws {
+        try await database.schema("users")
+            .id()
+            .field("created", .string)
+            .field("modified", .string)
+            .field("login_id", .string)
+            .field("password", .string)
+            .unique(on: "login_id")
+            .create()
+    }
+    
+    func revert(on database: Database) async throws {
+        try await database.schema("users").delete()
+    }
+}
